1f2413b2c3ddbaba0a2ae7c376977e04816726a5
Your Name <sharmahemantoh@gmail.com>
2019-04-09 15:05:25 +0100

    Second API

--- api/routes/menu.js
+++ api/routes/menu.js
@@ -13,4 +13,30 @@ router.post('/', (req, res, next) => {
     })
 })
 
+router.get('/:menuId',(req, res, next) => {
+    const id = req.params.menuID;
+    if (id === 'special'){
+        res.status(200).json({
+            message: 'You discovered the special ID',
+            id: id
+        })
+    } else {
+        res.status(200).json({
+            message: 'You passed an ID'
+        })
+    }
+})
+
+router.patch('/:menuId',(req, res, next) => {
+    res.status(200).json({
+        message: 'Updated menu'
+    })
+})
+
+router.delete('/:menuId',(req, res, next) => {
+    res.status(200).json({
+        message: 'Deleted menu'
+    })
+})
+
 module.exports = router;
