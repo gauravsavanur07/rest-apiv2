a122b6315c7f709bf6d9e123e6f1636de278b4c1
Your Name <sharmahemantoh@gmail.com>
2019-04-11 09:00:11 +0100

    GET method using an ID

--- api/routes/menu.js
+++ api/routes/menu.js
@@ -5,12 +5,19 @@ const router = express.Router();
 //importing the mongoose
 const mongoose = require('mongoose');
 
-//modelling the variable menu
+//modeling the variable menu
 const Menu = require('../models/menu'); 
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Handling GET requests to /menu'
+    Menu.find().exec().then(docs => {
+        console.log(docs);
+        res.status(200).json(docs);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        })
     })
 })
 router.post('/', (req, res, next) => {
@@ -24,26 +31,40 @@ router.post('/', (req, res, next) => {
     //adds to the database (mongoose method)
     menu.save().then(result => {
         console.log(result);
-    });
-    res.status(200).json({
-        message: 'Handling POST requests to /menu',
-        createdMenu: menu
+        res.status(201).json({
+            message: 'Handling POST requests to /menu',
+            createdMenu: result
+        });
     })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err 
+        });
+    })
+   
 })
 
-router.get('/:menuId',(req, res, next) => {
-    const id = req.params.menuID;
-    if (id === 'special'){
-        res.status(200).json({
-            message: 'You discovered the special ID',
-            id: id
-        })
-    } else {
-        res.status(200).json({
-            message: 'You passed an ID'
-        })
-    }
+router.get('/:menuId', (req, res, next) => {
+    const id = req.params.menuId;
+    Menu.findById(id)
+    .exec()
+    .then(doc => {
+        console.log("From the database", doc);
+        if (doc) {
+            res.status(200).json(doc);
+        } else {
+            res.status(404).json({message: 'No valid entry found for the given ID'});
+        }
+        
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({error: err});
+    })   
+
 })
+
 
 router.patch('/:menuId',(req, res, next) => {
     res.status(200).json({
